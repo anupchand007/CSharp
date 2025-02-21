592cb8bf850a31cf1c92182813576525fc728d0a
anupchand007 <2078bit@gmail.com>
2025-02-21 09:45:54 +0545

    Interface and Interface descrption: it allows multiple class to be inherited

---
+++ Chapter_WISE/Chapter_WISE/Interface.cs
@@ -0,0 +1,79 @@
+﻿namespace Chapter_WISE
+{
+    internal class Interfaces
+    {
+        interface IPrey
+        {
+            public void Run();      //decalaring method but not implementating
+
+        }
+        interface IPredator
+        {
+            public void Hunt();     //decalaring method but not implementating
+        }
+
+        class Rabbit : IPrey        //Rabbit is a prey which is hunt by many animals
+        {
+            public void Run()
+            {
+                Console.WriteLine("The Rabbit runs to safety");
+            }
+        }
+
+        class Hawk : IPredator      //Hawk is a Predator which is hunts smaller animals
+        {
+            public void Hunt()
+            {
+                Console.WriteLine("The Hawk is searching for Food");
+            }
+        }
+
+        class Fish : IPrey, IPredator       //the fish is a prey as well as hunter so, It sometimes run and hunts;
+        {
+
+            public void Run()
+            {
+                Console.WriteLine("The Fish runs aways from Bigger fish");
+            }
+
+            public void Hunt()
+            {
+                Console.WriteLine("The Fish hunts the smaller fish");
+            }
+        }
+
+
+        class InterfaceProgram
+        {
+            static void Main()
+            {
+                Rabbit rabbit = new Rabbit();
+                rabbit.Run();
+                //rabbit.Hunt();      //we will get error it only inhertis one class i.e IPrey
+
+                Hawk hawk = new Hawk();
+                //hwak.Run();         //we will get error it only inhertis one class i.e IPredator
+                hawk.Hunt();
+
+                Fish fish = new Fish();     // in this well can use both classes with the help of Interface
+                fish.Run();
+                fish.Hunt();
+            }
+        }
+
+
+
+        //interface IAnimal
+        //{
+        //    public void Speak();
+        //}
+
+        //class Dog: IAnimal { 
+        //public void Speak()
+        //    {
+        //        Console.WriteLine("Barks");
+        //    }
+
+        //}
+    }
+}
