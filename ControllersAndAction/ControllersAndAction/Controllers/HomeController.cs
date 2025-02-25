ca4a0b6eb97a57162072e2eae4b085b50ad5ce82
anupchand007 <2078bit@gmail.com>
2025-02-25 18:59:34 +0545

    Updates

--- ControllersAndAction/ControllersAndAction/Controllers/HomeController.cs
+++ ControllersAndAction/ControllersAndAction/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ControllersAndAction.Controllers
-{ 
+{
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            return View();  
+            //Note: IActionResult is inherited by many child classes  for eg: ViewResult, PartialViewResult, JsonResult etc. So it can return various type of data
         }
+
+        public string DisplayString()
+        {
+            return "Welcome Beatuiful word";
+        }
+
+        public int DisplayId(int id)
+        {
+            return id;
+           
+        }
+
+
     }
 }
--- ControllersAndAction/ControllersAndAction/obj/Debug/net8.0/ControllersAndAction.AssemblyInfo.cs
+++ ControllersAndAction/ControllersAndAction/obj/Debug/net8.0/ControllersAndAction.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControllersAndAction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7289274cb9afc1e1bfd4fe74cc9eca9263160b01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43f2bd2eab186e52a84b9d931842c175ba5c4819")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControllersAndAction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControllersAndAction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
