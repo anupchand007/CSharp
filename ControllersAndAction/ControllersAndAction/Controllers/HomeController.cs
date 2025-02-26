97d11f3ae06c71cffafe8d7a00f58a7e449245d6
anupchand007 <2078bit@gmail.com>
2025-02-26 20:20:17 +0545

    Updates

--- ControllersAndAction/ControllersAndAction/Controllers/HomeController.cs
+++ ControllersAndAction/ControllersAndAction/Controllers/HomeController.cs
@@ -5,21 +5,75 @@ namespace ControllersAndAction.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index()
+
         {
-            return View();  
+            //To Passes Data from controller to view we can use various methods
+
+            //1. ViewData
+
+            //ViewData["Name"] = "Anup Chand";
+            //ViewData["Age"] = 22;
+            //ViewData["Time"] = DateTime.Now;
+
+            //string[] arr = { "Anup", "Bimal" };
+            //ViewData["Array"] = arr;
+
+            //ViewData["Games"] = new List<string>{
+            //    "Valorant", "Alan Wake", "Last of Us"
+            //};
+
+            //2. ViewBag
+
+            //ViewBag.data1 = "Learn .Net";
+            //ViewBag.data2 = 21;
+            //ViewBag.data3 = DateTime.Now.ToShortTimeString();
+
+
+            //string[] arrs = { "Anup", "Bimal", "Sumit", "Ananta" };
+            //ViewBag.data4 = arrs;
+
+            //ViewBag.data5 = new List<string> {
+
+            // "Valorant", "Alan Wake", "Last of Us"
+
+            //};
+
+            //1-2 Interchangable 
+
+            //ViewData["data"] = "This is the data from ViewData[] being accesed by ViewBag";
+
+            //ViewBag.Me = "It me anup chand, Data of ViewBag but being accessed and Printed using ViewData Method";
+
+            //3. TempData
+
+            ViewData["data1"] = "View Data";
+            ViewBag.data2 = "View Bag";
+            TempData["data3"] = "Temp Data";
+
+            TempData["Games"] = new List<string> { "Valorant", "Alan Wake", "Last of Us" };
+
+
+            TempData.Keep("data3");        //keeps the value for 2nd or 3rd request
+            return View();
             //Note: IActionResult is inherited by many child classes  for eg: ViewResult, PartialViewResult, JsonResult etc. So it can return various type of data
         }
 
-        public string DisplayString()
+        public IActionResult About()
         {
-            return "Welcome Beatuiful word";
+            TempData.Keep("data3");
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            TempData.Keep("data3");
+            return View();
         }
 
-        public int DisplayId(int id)
-        {
-            return id;
-           
-        }
+        //public int DisplayId(int id)
+        //{
+        //    return id;
+
+        //}
 
 
     }
--- ControllersAndAction/ControllersAndAction/obj/Debug/net8.0/ControllersAndAction.AssemblyInfo.cs
+++ ControllersAndAction/ControllersAndAction/obj/Debug/net8.0/ControllersAndAction.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControllersAndAction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43f2bd2eab186e52a84b9d931842c175ba5c4819")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+123e7951bc55807256d9fd2592a07ce40594695b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControllersAndAction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControllersAndAction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
